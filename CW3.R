6f78515185b23a987bab191003a6c6800e9dfdeb
Tomasz Szymanski <t_szymanski@onet.eu>
2019-12-30 15:55:08 +0100

    zadanie3 - initial commit

---
+++ CW3.R
@@ -0,0 +1,13 @@
+#####################################################################################
+##                    ZADANIE 1                                                 #####
+#####################################################################################
+  
+
+#1. Zbiór danych Vehicle z pakietu mlbench zawiera informacje (18 cech, 846 obserwacji)
+# o sylwetkach samochodów (4 typy).
+# (a) Skonstruuj modele klasyfikacji: LDA, QDA, 1NN & NB dla tych danych.
+# (b) Oszacuj b≥πd klasyfikacji dla skonstruowanych metod.
+# (c) Który ze skonstruowanych klasyfikatorów jest godny polecenia dla tego zbioru danych?
+#  Odpowiedü uzasadnij.
+#(d*) Spróbuj zwizualizowaÊ wyniki w przestrzeni utworzonej przez dwie pierwsze sk≥adowe
+#g≥ówne.
